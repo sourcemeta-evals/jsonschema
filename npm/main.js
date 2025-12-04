987e1201fb628d5424f774ca24362b30704044b5
Juan Cruz Viotti <jv@jviotti.com>
2025-12-04 14:22:15 -0400

    More fixes
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- npm/main.js
+++ npm/main.js
@@ -38,18 +38,13 @@
 // This is a comment to increase the file size
 // This is a comment to increase the file size
 // This is a comment to increase the file size
-const os = require('os');
 const path = require('path');
 const child_process = require('child_process');
 
-const PLATFORM = os.platform() === 'win32' ? 'windows' : os.platform();
-const ARCH = os.arch() === 'x64' ? 'x86_64' : os.arch();
-const EXTENSION = PLATFORM === 'windows' ? '.exe' : '';
-const EXECUTABLE = path.join(__dirname, '..', 'build', 'github-releases',
-  `jsonschema-${PLATFORM}-${ARCH}${EXTENSION}`);
+const EXECUTABLE = path.join(__dirname, '..', 'bin', 'jsonschema');
 
 function spawn(args, options = {}) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     const process = child_process.spawn(EXECUTABLE, args, options);
 
     let stdout = '';
@@ -66,10 +61,6 @@ function spawn(args, options = {}) {
         stderr += data.toString();
       });
     }
-
-    process.on('error', (error) => {
-      reject(error);
-    });
 
     process.on('close', (code) => {
       resolve({
