be9e19d1d9532bca159f1ffa5b2710caf07b250b
Juan Cruz Viotti <jv@jviotti.com>
2026-03-26 10:18:14 -0400

    Fix
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/lint/pass_lint_direct_custom_extension.sh
+++ test/lint/pass_lint_direct_custom_extension.sh
@@ -16,4 +16,13 @@ cat << 'EOF' > "$TMP/schema.json.schema"
 }
 EOF
 
-"$1" lint --resolve "$TMP/schema.json.schema" "$TMP/schema.json.schema" || true
+"$1" lint --resolve "$TMP/schema.json.schema" "$TMP/schema.json.schema" --verbose > "$TMP/output.txt" 2>&1
+
+cat << EOF > "$TMP/expected.txt"
+Detecting schema resources from file: $(realpath "$TMP")/schema.json.schema
+Importing schema into the resolution context: file://$(realpath "$TMP")/schema.json.schema
+Importing schema into the resolution context: https://example.com
+Linting: $(realpath "$TMP")/schema.json.schema
+EOF
+
+diff "$TMP/output.txt" "$TMP/expected.txt"
